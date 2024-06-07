a0b6f2b610b95f72fdc931c8675743727fa17884
dalilakrslak <dkrslak1@etf.unsa.ba>
2024-06-07 19:24:38 +0200

    changed sos button

--- components/SideButtons.tsx
+++ components/SideButtons.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, Dimensions, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-const SideButtons = ({ onChatPress, onSOSPress }:  {onChatPress:any, onSOSPress:any}) => {
-  
+const SideButtons = ({ onChatPress, onSOSPress }: { onChatPress: any; onSOSPress: any }) => {
   return (
     <View style={styles.container}>
       {/* Chat button */}
@@ -15,17 +14,16 @@ const SideButtons = ({ onChatPress, onSOSPress }:  {onChatPress:any, onSOSPress:
       </TouchableOpacity>
       {/* SOS button */}
       <TouchableOpacity style={[styles.button, { backgroundColor: '#FF5733' }]} onPress={onSOSPress}>
-        <Ionicons name="alert-circle-outline" size={windowWidth * 0.1} color="white" style={styles.sosIcon} />
+        <Text style={styles.sosText}>SOS</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
-  
 const styles = StyleSheet.create({
   container: {
     position: 'absolute',
-    bottom: windowHeight * 0.05,
+    bottom: windowHeight * 0.03,
     right: windowWidth * 0.05,
     alignItems: 'flex-start',
     justifyContent: 'center',
@@ -39,8 +37,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: 10,
   },
-  sosIcon: {
-    paddingLeft: 3, // Adjust the padding to move the icon to the right
+  sosText: {
+    fontSize: windowWidth * 0.05,
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
